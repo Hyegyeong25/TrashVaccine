7b2f2289177ac08e6ed3bd42c9e81937c1616ed7
Joohyegyeong <hyegyeong25@gmail.com>
2022-07-22 00:27:29 +0900

    commit test

---
+++ src/main/java/kopo/poly/controller/indexController.java
@@ -0,0 +1,18 @@
+package kopo.poly.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
+@Controller
+public class indexController {
+    @GetMapping(value = "index")
+    public String indexPage() throws Exception {
+        log.info(this.getClass().getName() + ".indexPage Start !!");
+        log.info(this.getClass().getName() + ".indexPage End !!");
+        return "index";
+    }
+}
