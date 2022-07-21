7bf8e1c861331ae419ceef29bb76eebcd2bdd58a
hyegyeong25 <hyegyeong25@gmail.com>
2022-07-22 01:22:37 +0900

    commit all

---
+++ src/main/java/kopo/poly/TrashVaccineApplication.java
@@ -0,0 +1,13 @@
+package kopo.poly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrashVaccineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrashVaccineApplication.class, args);
+    }
+
+}
